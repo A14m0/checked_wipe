9a9ede0fc9986ad012e0e60a6ad174edabc9f94a
Probably a Penguin <nicolas.p.ammann@gmail.com>
2021-07-02 00:36:09 -0600

    User sanity checks implemented

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use std::{
         pattern::Pattern,
     },
     io::{
-        self, 
-        BufRead
+        self,
+        prelude::*, 
+        BufRead,
     },
     path::Path,
     fs::File,
@@ -36,6 +37,10 @@ struct DiskData {
     partitions: Vec<PartitionData>
 }
 
+
+
+
+
 /// helper function for reading lines from a file
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -61,6 +66,85 @@ fn is_mounted(id: String) -> bool {
     false
 }
 
+/// attempts to mount the partition at `PATH` to `/tmp/mnt`
+fn try_mount(path: String) -> Result<u32, String> {
+    // make sure the directory `/tmp/mnt` exists, if not create it
+    match std::fs::read_dir("/tmp/mnt") {
+        Ok(_) => (),
+        Err(_) => match std::fs::create_dir("/tmp/mnt"){
+            Ok(_) => (),
+            Err(e) => return Err(format!("Failed to mount drive: {}", e))
+        } 
+    }
+
+    // try to mount the drive to the point
+    match Command::new("mount").arg(&path[..]).arg("/tmp/mnt").status() {
+        Ok(a) => {
+            if !a.success(){
+                match a.code(){
+                    Some(code) => {
+                        // 32 means unknown fs, we dont want to die on this, 
+                        // so we will just print that we dont know the fs
+                        if code != 32 {
+                            return Err(format!("mount failed with {}", a));
+                        } else {
+                            println!("\t    Unknown partition type");
+                            return Ok(1)
+                        }
+                    },
+                    None => return Err("mount was terminated by a signal".to_string())
+                }
+            }
+        },
+        Err(e) => return Err(format!("Failed to run mount command: {}", e))
+    };
+    
+    Ok(0)   
+}
+
+/// unmounts the partition on `/tmp/mnt`
+fn unmount() -> Result<(), String> {
+    // try to unmount the drive
+    match Command::new("umount").arg("/tmp/mnt").status() {
+        Ok(a) => {
+            if !a.success(){
+                return Err(format!("umount failed with {}", a));
+            }
+        },
+        Err(e) => return Err(format!("Failed to run umount command: {}", e))
+    };
+    
+    Ok(())
+}
+
+/// prints all of the top level directories of each partition on `disk`
+fn print_top_levels(disk: &DiskData) -> Result<(), String>{
+    let mut ctr = 1;
+    for partition in disk.partitions.iter(){
+        println!("\tPartition #{}", ctr);
+        let status = match try_mount(partition.id.clone()){
+            Ok(a) => a,
+            Err(e) => return Err(e)
+        };
+        
+        // we ignore partitions that return 1, as they are unknown and 
+        // therefore not mounted to `/tmp/mnt`
+        if status == 0 {
+            let paths = std::fs::read_dir("/tmp/mnt").unwrap();
+            for path in paths {
+                println!("\t    {:?}", path.unwrap().file_name());
+            }
+            unmount()?;
+        }
+        ctr += 1;
+        
+    }
+
+    Ok(())
+}
+
+
+
 /// define functions for our structures
 impl PartitionData {
     fn new(part_line: String) -> Self {
@@ -77,7 +161,7 @@ impl PartitionData {
 }
 impl Display for PartitionData {
     fn fmt (&self, fmt: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-        write!(fmt, "Path: {}, Start: {}, End: {}, Size: {}, File system: {}, Mount state: {}",
+        write!(fmt, "{}, Start: {}, End: {}, Size: {}, File system: {}, Mount state: {}",
             self.id, self.start, self.end, self.size, self.fstype, self.is_mounted
         )
     }
@@ -101,7 +185,7 @@ impl DiskData {
 
 impl Display for DiskData {
     fn fmt (&self, fmt: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-        write!(fmt, "Path: {}, Size: {} {}, # Unmounted Partitions: {}",
+        write!(fmt, "Path: {}, Size: {} {}, # Partitions: {}",
             self.path, self.size, self.size_unit, self.partitions.len()
         )
     }
@@ -114,7 +198,7 @@ fn print_welcome(){
 }
 
 /// populated `drives_vec` with the currently unmounted available drives
-fn get_unmounted(drives_vec: &mut Vec<DiskData>) -> Result<(), String> {
+fn parse_partitions(drives_vec: &mut Vec<DiskData>) -> Result<(), String> {
     println!("Reading partition information...");
     // get fdisk's drive listing
     let system_output = match Command::new("fdisk").arg("-l").output() {
@@ -139,12 +223,11 @@ fn get_unmounted(drives_vec: &mut Vec<DiskData>) -> Result<(), String> {
             if disk.path.is_prefix_of(tmp_str){
                 let partition = PartitionData::new(tmp_str.to_string());
                 // disregard any partitions that are mounted
-                if !partition.is_mounted{
-                    match disk.add_partition(partition.clone()){
-                        Ok(_) => (),
-                        Err(e) => panic!("Failed to add partition to structure: {}", e)
-                    };
-                }
+                match disk.add_partition(partition.clone()){
+                    Ok(_) => (),
+                    Err(e) => panic!("Failed to add partition to structure: {}", e)
+                };
+                
             }
         }
 
@@ -155,66 +238,89 @@ fn get_unmounted(drives_vec: &mut Vec<DiskData>) -> Result<(), String> {
     Ok(())
 }
 
-/* Sample FDISK output
-
-
-Disk /dev/nvme0n1: 931.51 GiB, 1000204886016 bytes, 1953525168 sectors
-Disk model: CT1000P1SSD8                            
-Units: sectors of 1 * 512 = 512 bytes
-Sector size (logical/physical): 512 bytes / 512 bytes
-I/O size (minimum/optimal): 512 bytes / 512 bytes
-Disklabel type: gpt
-Disk identifier: B381325B-40D1-5643-83B0-4C5C67AC51F5
-
-Device            Start        End    Sectors  Size Type
-/dev/nvme0n1p1     2048    1050623    1048576  512M EFI System
-/dev/nvme0n1p2  1050624   68159487   67108864   32G Linux swap
-/dev/nvme0n1p3 68159488 1953525134 1885365647  899G Linux filesystem
-
-
-Disk /dev/nvme1n1: 931.51 GiB, 1000204886016 bytes, 1953525168 sectors
-Disk model: CT1000P1SSD8                            
-Units: sectors of 1 * 512 = 512 bytes
-Sector size (logical/physical): 512 bytes / 512 bytes
-I/O size (minimum/optimal): 512 bytes / 512 bytes
-Disklabel type: gpt
-Disk identifier: 5E46032F-79DF-604D-8BF0-1A47542AE033
-
-Device              Start        End    Sectors   Size Type
-/dev/nvme1n1p1       2048     206847     204800   100M EFI System
-/dev/nvme1n1p2     206848     239615      32768    16M Microsoft reserved
-/dev/nvme1n1p3     239616 1952485492 1952245877 930.9G Microsoft basic data
-/dev/nvme1n1p4 1952487424 1953521663    1034240   505M Windows recovery environm
-
-
-Disk /dev/sda: 3.64 TiB, 4000787030016 bytes, 7814037168 sectors
-Disk model: WDC WD4005FZBX-0
-Units: sectors of 1 * 512 = 512 bytes
-Sector size (logical/physical): 512 bytes / 4096 bytes
-I/O size (minimum/optimal): 4096 bytes / 4096 bytes
-Disklabel type: gpt
-Disk identifier: 11B1E966-CA57-CF40-9E7B-602311EBF91C
-
-Device     Start        End    Sectors  Size Type
-/dev/sda1   2048 7814037134 7814035087  3.6T Linux filesystem
-
-*/
 
 fn main() {
     print_welcome();
     // check we are running as root
     if !Uid::effective().is_root() {
-        panic!("You must run this executable with root permissions");
+        panic!("[-] This program must be run as root");
     }
 
 
     let mut drives_vec: Vec<DiskData> = Vec::new();
-    get_unmounted(&mut drives_vec).expect("Failed to read drives");
+    parse_partitions(&mut drives_vec).expect("Failed to read drives");
 
+    println!("All Drives ___________________________________________________");
     for drive in drives_vec.iter(){
-        println!("{}", drive);
+        println!("\t{}", drive);
         for partition in drive.partitions.iter(){
-            println!("\t{}", partition);
+            println!("\t\t{}", partition);
         }
     }
+
+    println!("\nAll Drives Currently Unmounted _______________________________");
+    let mut umount_idx_vec: Vec<usize> = Vec::new();
+    let mut ctr = 0;
+    for drive in drives_vec.iter(){
+        let mut is_drive_mounted: bool = false;
+        for partition in drive.partitions.iter(){
+            if partition.is_mounted {
+                is_drive_mounted = true ;
+            }
+        }
+
+        // if the drive is not mounted, print it and save the index
+        if !is_drive_mounted {
+            println!("{}\t{}", ctr, drive);
+            umount_idx_vec.push(ctr);
+        }
+        ctr += 1;
+    }
+
+    println!("______________________________________________________________");
+    println!("Select the drive you would like to format (`q` to quit)");
+    let mut user_selection = -1;
+    let mut is_done = false;
+
+    // get the user's desired drive, either quitting or looping on character input
+    while !is_done {
+        let mut input_text = String::new();
+        print!(" > ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input_text).expect("failed to read from stdin");
+
+        let trimmed = input_text.trim();
+        match trimmed.parse::<i32>() {
+            // user gave proper selection
+            Ok(i) => {
+                // make sure the user isnt being an idiot
+                if i < 1 || i > umount_idx_vec.len() as i32 {
+                    println!("[-] Not a valid drive index. Please try again");
+                } else {
+                    user_selection = i;
+                    is_done = true
+                }
+                
+            },
+            Err(..) => {
+                if &trimmed[0..1] == "q" {
+                    println!("[ ] Caught quitting input. Doing so...");
+                    std::process::exit(0);
+                } else {
+                    println!("[-] Not a valid drive index. Please try again");
+                }
+            },
+        };
+    }
+
+    
+
+    println!("______________________________________________________________");
+    println!("You have selected disk # {}", user_selection);
+    println!("{}", drives_vec[umount_idx_vec[user_selection as usize-1]]);
+    match print_top_levels(&drives_vec[umount_idx_vec[user_selection as usize-1]]){
+        Ok(_) => (),
+        Err(e) => println!("Failed to print all the things: {}", e)
+    };
+    println!("Does this information look correct? (y/N)");
 }
