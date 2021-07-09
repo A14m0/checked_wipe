5311b8386f191599abdcf9da882ac1b8a7206afd
Probably a Penguin <nicolas.p.ammann@gmail.com>
2021-07-09 12:50:46 -0600

    Changed prints to progress bars

--- src/definitions/mod.rs
+++ src/definitions/mod.rs
@@ -1,12 +1,11 @@
 use std::{
-    time::{
-        Duration, 
-        SystemTime
-    },
     io::{
-        self,
         prelude::*,
     }
+};
+use indicatif::{
+    ProgressBar,
+    ProgressStyle
 };
 
 mod defs;
@@ -22,13 +21,16 @@ pub fn zero_drive(disk: &DiskData) -> Result<(), String> {
     // open the file and prep variables
     let mut drive_handle = std::fs::File::create(disk.path.clone()).expect("Failed to open disk for writing");
     let write_buf: [u8; 1024*1024] = [0;1024*1024];
-    let mut total_written = 0u32;
-    let percent_denom = disk.size * ((1024 * 1024) as f64);
-    let mbs = 1024*1000;
+    
+    // initialize a progress bar
+    let bar = ProgressBar::new(disk.size as u64);
+    bar.set_style(ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] {bar:40.red/cyan} {pos:>7}/{len:7} GB")
+                .progress_chars("##-"));
     
     // loop until disk is fully written
-    for idx in 0..disk.size as u64 {
-        let now = SystemTime::now();
+    for _ in 0..disk.size as u64 {
+        //let now = SystemTime::now();
         // for each gigabyte...
         for _idx2 in 0..1024{
             match drive_handle.write_all(&write_buf){
@@ -37,26 +39,14 @@ pub fn zero_drive(disk: &DiskData) -> Result<(), String> {
             };
         }
 
-        // update measured stats
-        total_written += 1024*1024; // written 1 mb
-        let elapsed = match now.elapsed(){
-            Ok(a) => a,
-            Err(e) => {
-                println!("[-] Failed to time the write transactions: {}", e);
-                Duration::from_secs(0)
-            }
-        };
-
-        // clear line and print updates
-        print!("                                                                \r");
-        io::stdout().flush().unwrap();
-        print!("Written {} GB, {:.2}% completed ({} MB/s)\r", idx, (total_written*100) as f64/percent_denom, mbs/elapsed.as_millis());
-        io::stdout().flush().unwrap();
+        // increment the progress bar
+        bar.inc(1);
 
         // need to flush the drive file so we dont pretend we 
         // are writing faster than we actually are
         drive_handle.flush().unwrap(); 
     }
+    bar.finish();
     
     Ok(())
 }
